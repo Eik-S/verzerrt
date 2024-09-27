eb34375a27398b3c1a64a697749bcd0e6f030699
Bagrat Akopyan <bagrat.akopyan@gmail.com>
2024-09-27 09:36:48 +0200

    Add faz logo

--- verzerrt-website/src/components/description.tsx
+++ verzerrt-website/src/components/description.tsx
@@ -8,6 +8,7 @@ export function Description({ ...props }) {
         Online-Zeitungen sammeln Kartoffelpunkte wenn ihre Berichterstattung über Personengruppen
         von deren gesellschaftlichem Anteil abweicht.
       </p>
+      ---
       <p css={styles.text}>
         Warp gangway Jolly Roger crimp Letter of Marque cackle fruit knave blow the man down long
         clothes swab. Transom warp Pirate Round handsomely Pieces of Eight prow bowsprit clipper aye
@@ -19,6 +20,12 @@ export function Description({ ...props }) {
         her cable Nelsons folly no prey, no pay brigantine. Boatswain Corsair tack take a caulk hulk
         prow Cat o'nine tails hail-shot long boat Jack Tar. Cable overhaul Plate Fleet spike
         broadside brig grog blossom clipper aye loaded to the gunwalls.
+      </p>
+
+      ---
+
+      <p css={styles.contact}>
+        Contact: <a href="mailto:post@kartoffelindex.de">post@kartoffelindex.de</a>
       </p>
     </div>
   )
@@ -37,5 +44,13 @@ const styles = {
   text: css`
     font-size: 18px;
     letter-spacing: 0.04em;
+  `,
+  contact: css`
+    font-size: 14px;
+    color: #888;
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
   `,
 }
